1687c32866aa8009ce6b12d2ce25befa70e1906a
Tony Huynh <huynh.tony55@gmail.com>
2019-09-02 11:02:48 +1000

    refactored challenge 2

--- 02_largestNumber.js
+++ 02_largestNumber.js
@@ -13,7 +13,7 @@ with no changes to the function?
 function largestNumber(arr) {
     let largest = arr[0];
 
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) {
         if (largest < arr[i] ) {
             largest = arr[i];
         }
