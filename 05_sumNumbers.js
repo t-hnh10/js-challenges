9b46e920c5ffb1e56f42e90abe806d962b04911c
Tony Huynh <huynh.tony55@gmail.com>
2019-09-04 11:05:55 +1000

    completed challenge 5

--- 05_sumNumbers.js
+++ 05_sumNumbers.js
@@ -2,9 +2,26 @@
 // and return the name of the player with the highest total score.
 // Test your solution:
 // npm test
+function calculateTotalScore(scores) {
+    // Calculates the total of the array of scores that is passed in
+    let totalScore = 0;
+    for (let score of scores) {
+        totalScore += score;
+    }
+    return totalScore;
+}
 
 function findWinner(players) {
-    // Your code here
+    let winner = players[0].name;
+    let winnerScore = calculateTotalScore(players[0].scores);
+    for (let ind = 1; ind < players.length; ind++) {
+        let playerTotalScore = calculateTotalScore(players[ind].scores)
+        if (playerTotalScore > winnerScore) {
+            winner = players[ind].name;
+            winnerScore = playerTotalScore;
+        }
+    }
+    return winner.name;
 }
 
 module.exports = {
