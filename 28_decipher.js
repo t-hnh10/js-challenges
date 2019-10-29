187ab72c21267b041fa5d5e926970f4ea757d634
Tony Huynh <huynh.tony55@gmail.com>
2019-10-29 10:28:09 +1000

    added two solutions

--- 28_decipher.js
+++ 28_decipher.js
@@ -17,6 +17,46 @@ function decipher(code) {
 	// your code goes here
 }
 
+function decipher(code) {
+    let newCode="";
+    var n
+	var words = code.split(" ");
+    words.forEach(word => {
+        newWord="";
+        let number;
+        if (word.charAt(0)=='1'){
+            n=3
+        }   
+        else{
+            n=2
+        }
+        
+        number = parseInt(word.substring(0,n));
+        newWord = String.fromCharCode(number)
+        if (word.length>(n+1))
+            newWord+=word.charAt(word.length - 1)
+        if (word.length>(n+2))
+            newWord+=word.substring((n+1),word.length - 1)
+        newWord+=word.charAt(n)
+       
+        if (newCode===""){
+            newCode=newWord;
+        }else{
+            newCode+=" "+newWord;
+        }
+    });
+    console.log(newCode)
+}
+
+function decipher(code) {
+	return code.split(' ').map(word => {
+		let firstLetter = String.fromCharCode(word.match(/\d+/));
+		word = word.match(/[a-z]/gi) || [];
+		[word[0], word[word.length - 1]] = [word[word.length - 1], word[0]];
+		return firstLetter + word.join('');
+	}).join(' ');
+}
+
 let assert = require("assert")
 describe("decipher", () => {
 	it("should decipher '72olle 103doo 100ya' as 'Hello good day'", () => {
